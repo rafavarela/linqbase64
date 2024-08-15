38b7b61cdcf3d4eeadc214b12323fb888ac8ad6d
Rafael Varela <rafa_varela@yahoo.com>
2024-08-14 20:24:00 -0500

    first commit

---
+++ base64/Model/Persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace base64.Model
+{
+    public class Persona
+    {
+        public string id { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string sexo { get; set; }
+        public string email { get; set; }
+        public Double salario { get; set; }
+        public DateTime fechaNacimiento { get; set; }
+        public bool soltero { get; set; }
+        public string pais { get; set; }
+        public string estado { get; set; }
+        public string ciudad { get; set; }
+        public string direccion { get; set; }
+        public string codigoPostal { get; set; }
+        public string telefono { get; set; }
+        public string profesion { get; set; }
+
+    }
+}
---
+++ base64/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+using Newtonsoft.Json;
+using base64.Model;
+
+
+namespace base64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pasar texto del archivo a una variable
+            string encodedString = ObtenerCadenaDeArchivo(AppDomain.CurrentDomain.BaseDirectory + "stringBase64_v1.txt");
+
+            // Obtener cadena decodificada
+            string decodedString = DecodificarBase64(encodedString);
+
+            // Transformar cadena en objeto Json
+            var personas = ConvertirStringEnJson(decodedString);
+
+            // Mujeres casadas mayores de 50 que viven en Miami
+            var q1 = personas
+                        .Where( p => 
+                                p.sexo == "F" && 
+                                !p.soltero && 
+                                (DateTime.Today.Year - p.fechaNacimiento.Year) > 50 &&
+                                p.ciudad == "Miami" )
+                        .Count();
+
+
+            // En el estado de Texas cual es el sueldo promedio de los profesores
+            var q2 = personas
+                        .Where(p =>
+                               p.estado == "Texas" &&
+                               p.profesion == "Teacher"
+                        )
+                        .Average(p => p.salario);
+
+
+            // Cual es la ciudad con mas personas
+            var q3 = (personas
+                        .GroupBy(p => p.ciudad)
+                        .Select(c => new { ciudad = c.Key, cantidad = c.Count() }))
+                        .OrderByDescending(city => city.cantidad)
+                        .First();
+
+
+            // Impresión de resultados
+            Console.WriteLine("Mujeres casadas mayores de 50 que viven en Miami: {0}", q1);
+            Console.WriteLine("Sueldo promedio de profesores de Texas: {0}", q2);
+            Console.WriteLine("Ciudad con más personas: {0}. Cantidad {1}", q3.ciudad, q3.cantidad);
+            
+            Console.Read();
+        }
+
+
+        private static string ObtenerCadenaDeArchivo(string RutaArchivoTXT)
+        {
+            string textoArchivo = System.IO.File.ReadAllText(RutaArchivoTXT);
+            return textoArchivo;
+        }
+
+        private static string DecodificarBase64(string encodedString)
+        {
+            var bytes = Convert.FromBase64String(encodedString);
+            string decodedString = Encoding.UTF8.GetString(bytes);
+            return decodedString;
+        }
+
+        private static List<Persona> ConvertirStringEnJson(string stringPersonas)
+        {
+            return JsonConvert.DeserializeObject<List<Persona>>(stringPersonas);
+        }
+
+    }
+}
